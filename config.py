bcaaf294396d4d48181be0d6f98a68fab413ebaf
violet <lvhoa58@gmail.com>
2025-01-01 19:39:08 +0700

    add tgbot dockerfile

--- config.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv, find_dotenv
-import logging
-
-load_dotenv(find_dotenv())
-
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
